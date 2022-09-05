7fdf978af2777ad0bd76ed2de94c54a9cd8f5397
leon matias <roslin.pineda@nautilus.team>
2022-09-05 00:39:18 -0500

    added new example

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const optionMenu = document.querySelector(".select-menu"),
+    selectBtn = optionMenu.querySelector(".select-btn"),
+    options = optionMenu.querySelectorAll(".option"),
+    sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
+
+options.forEach(option =>{
+    option.addEventListener("click", ()=>{
+        sBtn_text.innerText = option.querySelector(".option-text").innerText;
+        optionMenu.classList.remove("active");
+    });
+});
